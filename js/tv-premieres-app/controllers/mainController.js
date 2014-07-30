4d1052f1e9743fbaf57f99b036538892260af6d6
Kevin Kirsche <kkirsche@ansfederal.com>
2014-07-30 11:34:47 -0400

    Connect to Trakt.TV API

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,6 +1,18 @@
 app.controller("mainController", function($scope, $http){
 
+    $scope.apiKey = "426e1c5fcd62e42599b3c1494052ea1e";
     $scope.init = function() {
+        //Trakt API requires requires a start date
+        var today = new Date();
+
+        //Create the date string and ensure leading zeros if they are required
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function(data) {
+            console.log(data);
+        }).error(function(error) {
+            console.log(error);
+        });
 
     };
 
